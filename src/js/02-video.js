9b9218c91fb81389d975e0ae22fb4d0e3105e34b
Atom1c69 <atomicc555@icloud.com>
2023-09-18 22:32:56 +0300

    check task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoPlayer = new Player('vimeo-player');
+
+const savePlayTime = throttle(function () {
+  videoPlayer.getCurrentTime().then(function (currentTime) {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}, 1000);
+
+videoPlayer.on('timeupdate', savePlayTime);
+
+videoPlayer.ready().then(function () {
+  const playTime = localStorage.getItem('videoplayer-current-time');
+
+  if (playTime) {
+    videoPlayer.setCurrentTime(playTime);
+  }
+});
