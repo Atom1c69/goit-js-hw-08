f50ddad62e7552668faac28237dd4bb9a5337ef9
Atom1c69 <atomicc555@icloud.com>
2023-09-20 13:54:33 +0300

    correction task 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,10 +2,11 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const videoPlayer = new Player('vimeo-player');
+const STORAGE_KEY = 'videoplayer-current-time';
 
 const savePlayTime = throttle(function () {
   videoPlayer.getCurrentTime().then(function (currentTime) {
-    localStorage.setItem('videoplayer-current-time', currentTime);
+    localStorage.setItem(STORAGE_KEY, currentTime);
   });
 }, 1000);
 
@@ -13,7 +14,7 @@ localStorage.setItem('local', 'test');
 videoPlayer.on('timeupdate', savePlayTime);
 
 videoPlayer.ready().then(function () {
-  const playTime = localStorage.getItem('videoplayer-current-time');
+  const playTime = localStorage.getItem(STORAGE_KEY);
 
   if (playTime) {
     videoPlayer.setCurrentTime(playTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,27 @@
 const form = document.querySelector('.feedback-form');
-const emailInput = form.querySelector('input[name="email"]');
-const messageInput = form.querySelector('textarea[name="message"]');
+// const emailInput = form.querySelector('input[name="email"]');
+// const messageInput = form.querySelector('textarea[name="message"]');
 const STORAGE_KEY = 'feedback-form-state';
 
 const saveFormState = throttle(() => {
-  const formState = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-
+  // const formState = {
+  //   email: emailInput.value,
+  //   message: messageInput.value,
+  // };
+  formState[e.target.name] = e.target.value.trim();
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
 }, 500);
 
 const loadFormState = () => {
-  const savedFormState = localStorage.getItem(STORAGE_KEY);
-
-  if (savedFormState) {
+  try {
+    const savedFormState = localStorage.getItem(STORAGE_KEY);
+    if (!savedFormState) return;
     const formState = JSON.parse(savedFormState);
-    emailInput.value = formState.email || '';
-    messageInput.value = formState.message || '';
+    Object.entries(formState).forEach(([key, val]) => {
+      form.elements[key].value = val;
+    });
+  } catch ({ message }) {
+    console.log(message);
   }
 };
 
@@ -28,13 +31,14 @@ form.addEventListener('submit', function (event) {
   event.preventDefault();
 
   localStorage.removeItem(STORAGE_KEY);
+  event.target.reset();
+  // emailInput.value = '';
+  // messageInput.value = '';
 
-  emailInput.value = '';
-  messageInput.value = '';
-
-  const formState = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
+  // const formState = {
+  //   email: emailInput.value,
+  //   message: messageInput.value,
+  // };
   console.log(formState);
+  formState = {};
 });
